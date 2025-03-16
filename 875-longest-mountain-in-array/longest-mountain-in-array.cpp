7e025ea40f5ca78cb9aaf9a27068ea4b830cd65a
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-03-16 14:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (96.59%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.cpp
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -1,28 +1,25 @@
 class Solution {
-private :
-    bool isMountain (int a , int b , int c){
-        return b > a && b > c ;
-    }
 public:
     int longestMountain(vector<int>& arr) {
         int n = arr.size();
-        int mx = 0;
-
-        for(int i = 1 ; i < n-1 ; i++){
-    
-            if(isMountain(arr[i-1] , arr[i] , arr[i+1])){
-                int l = i  - 1 ; 
-                int r = i  + 1; 
-                while(l > 0 && arr[l] > arr[l-1])
-                    --l;
-                while(r < n-1 && arr[r] > arr[r+1])
-                    r++;
-                mx = max(mx , r - l + 1 );
+        int ans=0;
+        int cnt=0;
+        for(int i=1;i<n-1;i++){
+            if(arr[i-1]<arr[i] && arr[i+1]<arr[i]){
+                cnt=1;
+                int j=i-1;
+                int k=i;
+                while(j>=0 && arr[j]<arr[j+1]){
+                    j--;
+                    cnt++;
+                }
+                while(k<n-1 && arr[k]>arr[k+1]){
+                    k++;
+                    cnt++;
+                }
             }
-
+            ans=max(ans,cnt);
         }
-
-        return mx;
-        
+        return ans;
     }
 };
