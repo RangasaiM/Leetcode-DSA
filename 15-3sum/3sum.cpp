ce129ca4628dd9e4c370a97e77fa9340da4b73a4
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-03-16 13:26:59 +0530

    Time: 51 ms (55.30%) | Memory: 29.2 MB (43.79%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i-1]==nums[i]) continue;
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k){
+                int val=nums[i]+nums[j]+nums[k];
+                if(val>0){
                     k--;
-                } else if (total < 0) {
+                }
+                else if(val<0){
                     j++;
-                } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
+                }
+                else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    k--;
                     j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+                    while(j<k && nums[j-1]==nums[j]) j++;
+                    while(j<k && nums[k+1]==nums[k]) k--;
                 }
             }
         }
-        return res;        
+        return ans;
     }
 };
