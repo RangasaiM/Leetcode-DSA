1c2203a514a595c283a67f235481a7e023aab359
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-04-09 15:59:20 +0530

    Time: 14 ms (37.11%) | Memory: 32.3 MB (57.55%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -4,22 +4,11 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]<k) return -1;
         }
-        set<int> st;
-        st.insert(k);
+        unordered_set<int> st;
         for(int i:nums){
-            st.insert(i);
+            if(i>k)
+                st.insert(i);
         } 
-        int cnt=st.size();
-        int l=st.size();
-        for(int i:st){
-            cout<<i<< " ";
-            if(i==k){
-                break;
-            }
-            cnt--;
-        }
-        // if(cnt==1 && l>1)
-        //     return -1;
-        return cnt-1;
+        return st.size();
     }
 };
