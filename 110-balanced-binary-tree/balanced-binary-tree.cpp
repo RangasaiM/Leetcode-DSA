c5fabb860ba6df1c36d8612056e6c5485831187c
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-08-11 19:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (56.42%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -14,14 +14,20 @@ public:
 
     int height(TreeNode* root){
         if(root==NULL) return 0;
-        return 1+max(height(root->left),height(root->right));
+        int l=height(root->left);
+
+        if(l==-1) return -1;
+
+        int r=height(root->right);
+        
+        if(r==-1) return -1;
+
+        if(abs(r-l)>1) return -1;
+
+        return 1+max(r,l);
     }
 
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
-        int l=height(root->left);
-        int r=height(root->right);
-        if(abs(r-l)>1) return false;
-        return isBalanced(root->left) && isBalanced(root->right);
+        return height(root)!=-1;
     }
 };
