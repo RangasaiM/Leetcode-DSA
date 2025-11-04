c00f8151c4cc86bdd0326f39c8501421e5062975
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-11-04 17:17:25 +0530

    Time: 184 ms (91.40%) | Memory: 10.5 MB (84.99%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& mat, string &s, int i, int j, int l, int n, int m, vector<vector<bool>> &vis) {
-    if(i < 0 || j < 0 || i >= n || j >= m || mat[i][j] != s[l] || vis[i][j]) 
+    bool dfs(vector<vector<char>>& mat, string &s, int i, int j, int l, int n, int m) {
+    if(i < 0 || j < 0 || i >= n || j >= m || mat[i][j] != s[l]) 
         return false;
     
     if(l == s.size() - 1) 
         return true;
 
-    vis[i][j] = true;
+    char temp=mat[i][j];
+    mat[i][j]='#';
 
-    bool found = dfs(mat, s, i - 1, j, l + 1, n, m, vis) ||
-                 dfs(mat, s, i + 1, j, l + 1, n, m, vis) ||
-                 dfs(mat, s, i, j - 1, l + 1, n, m, vis) ||
-                 dfs(mat, s, i, j + 1, l + 1, n, m, vis);
+    bool found = dfs(mat, s, i - 1, j, l + 1, n, m) ||
+                 dfs(mat, s, i + 1, j, l + 1, n, m) ||
+                 dfs(mat, s, i, j - 1, l + 1, n, m) ||
+                 dfs(mat, s, i, j + 1, l + 1, n, m);
 
-    vis[i][j] = false;
+    mat[i][j] = temp;
 
     return found;
 }
@@ -22,12 +23,11 @@ public:
     bool exist(vector<vector<char>>& mat, string s) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
     
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(mat[i][j] == s[0]) {
-                    if(dfs(mat, s, i, j, 0, n, m, vis))
+                    if(dfs(mat, s, i, j, 0, n, m))
                         return true;
                 }
             }
