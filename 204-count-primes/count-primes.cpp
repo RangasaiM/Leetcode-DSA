5f6e11abec4046c2f86aefff78fe33f6a1d11f77
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-12-11 14:04:30 +0530

    Time: 183 ms (56.18%) | Memory: 12.8 MB (37.21%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    vector<bool> getPrimes(int n){
+        vector<bool> primes(n+1,true);
+        primes[0]=primes[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(primes[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    primes[j]=false;
+                }
+            }
+        }
+        return primes;
+    }
+
+    int countPrimes(int n) {
+        vector<bool> primes=getPrimes(n);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(primes[i]){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
