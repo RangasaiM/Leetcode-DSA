b34423e188623a05fd9a398b4ca7a3318de7fdf1
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-12-18 16:54:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (37.31%) - LeetSync

---
+++ 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s, int k) {
+        unordered_map<char,int> mp;
+        int i=0,j=0;
+        long long cnt=0;
+        int n=s.length();
+        for (int i = 0; i < n; i++) {
+           mp[s[i]]++;
+
+           int maxi=0;
+           for(auto p:mp){
+            maxi=max(maxi,p.second);
+           }
+           
+           while(j<s.size() && maxi>=k){
+               cnt+=(n-i);
+               
+               mp[s[j]]--;
+               if(mp[s[j]]==0) mp.erase(s[j]);
+               j++;
+
+               maxi=0;
+
+               for(auto p:mp){
+                maxi=max(maxi,p.second);
+               }
+           }
+        }
+        return cnt;
+    }
+};
