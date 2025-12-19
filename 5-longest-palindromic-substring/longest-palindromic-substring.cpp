f1b742086760ce95f3df7fb50c99d40a8c753340
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-12-19 15:40:09 +0530

    Time: 88 ms (32.96%) | Memory: 174.3 MB (11.88%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
+
+    string expand(string s,int i,int j){
+        int n=s.length();
+        while(i>=0 && j<n && s[i]==s[j]){
+            i--;
+            j++;
+        }
+        return s.substr(i+1,j-i-1);
+    }
+
     string longestPalindrome(string s) {
         int n=s.size();
-        vector<vector<int>> dp(n,vector<int>(n));
-        int maxLen=0;
-        int st=0;
-        for(int i=0;i<n;i++) dp[i][i]=1;
-        for(int i=0;i<n-1;i++) {
-            if(s[i]==s[i+1]){
-             dp[i][i+1]=1;
-             maxLen=1;
-             st=i;
-            }
-            else dp[i][i+1]=0;
-        }
+        string ans="";
 
-        for(int i=2;i<n;i++){
-            for(int j=0;j<n-i;j++){
-                if(s[j]==s[j+i]){
-                    dp[j][j+i]=dp[j+1][j+i-1];
-                    if(dp[j][j+i]==1){
-                        maxLen=i;
-                        st=j;
-                    }
-                }
-                else dp[j][j+i]=0;
-            }
+        for(int i=0;i<n;i++){
+            string odd=expand(s,i,i);
+            if(ans.length()<odd.length()) ans=odd;
+            string even=expand(s,i,i+1);
+            if(ans.length()<even.length()) ans=even;
         }
-        return s.substr(st,maxLen+1);
+        return ans;
+        
     }
 };
