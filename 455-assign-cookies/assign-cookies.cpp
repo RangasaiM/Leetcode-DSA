4cba2f8163712ccd839d700b927f66842e6a0f8f
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2026-01-09 11:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (10.57%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int m=g.size();
+        int n=s.size();
+
+        int i=0;
+        int j=0;
+        int cnt=0;
+        while(i<n && j<m){
+            if(s[i]>=g[j]){
+                cnt++;
+                i++;
+                j++;
+            } 
+            else
+                i++;
+        }
+        return cnt;
+    }
+};
