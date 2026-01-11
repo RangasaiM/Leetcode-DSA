7d650d19c96c74c63d67067d1f1d32d181925199
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2026-01-11 20:38:43 +0530

    Time: 125 ms (28.89%) | Memory: 26 MB (33.63%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int n = s.length();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            unordered_map<char, int> freq;
-
-            for (int j = i; j < n; j++) {
-                freq[s[j]]++;
-
-                int maxi = INT_MIN;
-                int mini = INT_MAX;
-                for (auto it : freq) {
-                    mini = min(mini, it.second);
-                    maxi = max(maxi, it.second);
+        int n=s.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            unordered_map<char,int> mp;
+            for(int j=i;j<n;j++){
+                int mini=INT_MAX;
+                int maxi=INT_MIN;
+                mp[s[j]]++;
+                for(auto it:mp){
+                    mini=min(mini,it.second);
+                    maxi=max(maxi,it.second);
                 }
-                sum += (maxi - mini);
+            ans+=(maxi-mini);
             }
         }
-
-        return sum;
+        return ans;
     }
 };
